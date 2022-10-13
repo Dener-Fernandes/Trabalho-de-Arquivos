476a0b929236baeecbb2f69c430d887120c8dd52
Dener Fernandes de Oliveira <dener.fernandes.oliveira@gmail.com>
2022-10-12 22:57:29 -0400

    Create estruturas.h

---
+++ estruturas.h
@@ -0,0 +1,11 @@
+#define MAX 50
+
+struct tCursosEPesos {
+  int codigo;
+  char nome[MAX];
+  int red, mat, lin, hum, nat;
+};
+
+struct tCursosEVagas {
+  int codigo, ac, l1, l3, l4, l5, l7, l8, l9, l11, l13, l15;
+};
