542a600aa93794e665abf49457a5db6d2644f85a
Dener Fernandes de Oliveira <dener.fernandes.oliveira@gmail.com>
2022-10-14 00:04:44 -0400

    Update funcoesEntrada.h

--- funcoesEntrada.h
+++ funcoesEntrada.h
@@ -1,1 +1,8 @@
 // Aqui vai ter muita batatinha.
+#include "estruturas.h"
+#include <stdlib.h>
+
+void ler_e_inserir(char nomeArquivo01[MAX], char nomeArquivo02[MAX],
+                   char nomeArquivo03[MAX], char nomeArquivo04[MAX],
+                   tCursosEPesos *&cursosEPesos, tCursosEVagas *&cursosEVagas,
+                   tDados *&dados, tAcertos *&acertos);
