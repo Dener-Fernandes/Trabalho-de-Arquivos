e0cd3983e46239a16aed0439b797181d46e05d39
Dener Fernandes de Oliveira <dener.fernandes.oliveira@gmail.com>
2022-10-17 08:44:42 -0400

    Update main.cpp
    Funcionalidade de calcular média e desvio padrão adicionadas, além de uma funcionalidade imprimir para verificar as alocações.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Ordem de inserção dos arquivos: cursos_e_pesos.,
+// cursos_e_vagas.txt, dados.txt e acertos.txt
+
+void imprimir(tDados *lst) {
+  tDados *p;
+  for (p = lst; p != NULL; p = p->prox) {
+    printf("%d %d %d %s %d/%d/%d %s\n", p->codigo, p->qtdInscritos,
+           p->inscricao, p->nome, p->dia, p->mes, p->ano, p->vaga);
+  }
+
+  return;
+}
+
 int main() {
   tCursosEPesos *cursosEPesos = NULL;
   tCursosEVagas *cursosEVagas = NULL;
   tDados *dados = NULL;
   tAcertos *acertos = NULL;
+  tMediaAcertos mediaAcertos = {0.0};
+  tDesvioPadrao desvioPadrao = {0.0};
 
   // Variável que irá manipular o arquivo.
   FILE *arquivo;
@@ -22,6 +37,8 @@ int main() {
 
   ler_e_inserir(nome01, nome02, nome03, nome04, cursosEPesos, cursosEVagas,
                 dados, acertos);
+
+  calcularMediaEDesvioPadrao(acertos, &mediaAcertos, desvioPadrao);
 
   free(cursosEPesos);
   free(cursosEVagas);
